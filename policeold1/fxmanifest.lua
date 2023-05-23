15f0fdf1ffd0f8ba054a3485ec3266b0c4d310c0
Dennis Ikonomidis <132688618+DennisIkonomidis@users.noreply.github.com>
2023-05-23 05:43:12 +0200

    Add files via upload

---
+++ policeold1/fxmanifest.lua
@@ -0,0 +1,28 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'iBoss#2984'
+description 'Speciall Donate Cars from TheLuxEmpire discord.gg/theluxempire'
+repository 'https://github.com/iboss21'
+
+files {
+    'data/**/*.meta',
+    'data/**/*.xml',
+    'data/**/*.dat',
+    'data/**/*.ytyp'
+}
+
+data_file 'VEHICLE_METADATA_FILE'    'data/**/vehicles*.meta'
+
+
+
+
+
+
+
+
+
+
+
+
